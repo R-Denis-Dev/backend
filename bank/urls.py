638a27e6f6da7575597ace0d1e2d51a7aa2108cc
Denis Romanov <denisromanov601@gmail.com>
2026-04-24 14:56:23 +0300

    Update urls.py
    Created normal router in urls

--- bank/urls.py
+++ bank/urls.py
@@ -1,14 +1,13 @@
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (
-    TransactionViewSet,AuthenticationViewsets, TransferView,
-    MyNotification
-)
+from .views import AuthenticationViewSet, TransactionViewSet, TransferView, MyNotification
 
 router = DefaultRouter()
-router.register(r'auth',AuthenticationViewsets,'auth')
-router.register(r'transactions',TransactionViewSet,'transactions')
+router.register(r'auth', AuthenticationViewSet, basename='auth')
+router.register(r'transactions', TransactionViewSet, basename='transactions')
 
 urlpatterns = [
-
+    path('api/', include(router.urls)),
+    path('api/transfer/', TransferView.as_view(), name='transfer'),
+    path('api/notifications/', MyNotification.as_view(), name='notifications'),
 ]
